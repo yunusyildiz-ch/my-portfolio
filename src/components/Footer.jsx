5e2baf51c9669486974674ebe76deff653ed869c
yunusyildiz-ch <yunusyildizch@gmail.com>
2025-01-04 23:41:11 +0100

    build: website skeleton

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{ padding: "1rem", textAlign: "center", background: "#eee" }}
+    >
+      <p>© 2025 My Portfolio. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
