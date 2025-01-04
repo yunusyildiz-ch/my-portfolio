f685c53a2f6dd935f96280bbee58238798504155
yunusyildiz-ch <yunusyildizch@gmail.com>
2025-01-05 00:53:57 +0100

    add: project images

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const ProjectCard = () => {
+const ProjectCard = ({ imageUrl, title, description, link }) => {
   return (
     <div style={{ border: "1px solid #ccc", margin: "1rem", padding: "1rem" }}>
       <img src={imageUrl} alt={title} style={{ width: "100%" }} />
       <h3>{title}</h3>
       <p>{description}</p>
-      {link && <a href={link}>View Project</a>}
+      {link && <a href={link} target="_blank" rel="noopener noreferrer">View Project</a>}
     </div>
   );
 };
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,16 +6,18 @@ export const Projects = () => {
     {
       title: 'School Management System',
       description: 'A dynamic app with JS & REST API backend...',
-      imageUrl: '/images/school-project.png',
-      link: 'https://github.com/...'
+      imageUrl: '/images/project-school.png',
+      link :'https://josephfox-schoolmanagementsystem.netlify.app',
+      githublink: 'https://github.com/yunusyildiz-ch/School-Management-System-Project.git'
     },
     {
       title: 'Heating System Optimization',
       description: 'Full stack project with Arduino integration...',
-      imageUrl: '/images/heating-system.png',
-      link: 'https://github.com/...'
+      imageUrl: '/images/project-smartfox.png',
+      link : 'https://smartfoxhome.netlify.app',
+      githublink: 'https://github.com/yunusyildiz-ch/smartFOX_App.git'
     }
-    // ve benzeri...
+ 
   ]
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
@@ -26,6 +28,7 @@ export const Projects = () => {
           description={proj.description}
           imageUrl={proj.imageUrl}
           link={proj.link}
+          githublink={proj.githublink}
         />
       ))}
     </div>
