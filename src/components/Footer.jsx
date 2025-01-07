d863b4571d69c15443abd0ed5232ded30f80f9fe
yunusyildiz-ch <yunusyildizch@gmail.com>
2025-01-08 00:21:27 +0100

    update: footer  and projects page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="sidebar-footer">
       <ul className="footer-languages">
-        <li><Link to="#">English(US)</Link></li>
-        <li><Link to="#">Français</Link></li>
-        <li><Link to="#">Deutsch</Link></li>
-        <li><Link to="#">Italiano</Link></li>
-        <li><Link to="#">日本語</Link></li>
-        <li><Link to="#">Türkçe</Link></li>
-        <li><Link to="#">Русский</Link></li>
+        <li>
+          <Link to="#">English(US)</Link>
+        </li>
+        <li>
+          <Link to="#">Français</Link>
+        </li>
+        <li>
+          <Link to="#">Deutsch</Link>
+        </li>
+        <li>
+          <Link to="#">Italiano</Link>
+        </li>
+        <li>
+          <Link to="#">日本語</Link>
+        </li>
+        <li>
+          <Link to="#">Türkçe</Link>
+        </li>
+        <li>
+          <Link to="#">Русский</Link>
+        </li>
       </ul>
-      <p className="footer-copyright">Yunus YILDIZ &copy; 2025</p>
-      <p className="footer-address">Genève</p>
+      <div className="footer-texts">
+        <p className="footer-copyright">Yunus YILDIZ &copy; 2025</p>
+        <p className="footer-address">Genève</p>
+      </div>
     </footer>
   );
 };
