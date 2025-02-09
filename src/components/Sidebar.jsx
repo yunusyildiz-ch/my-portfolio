9e9d9fdfb128a0a85473b624394714b8ec92e038
yunusyildiz-ch <yunusyildizch@gmail.com>
2025-02-09 13:55:32 +0100

    update:sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,9 +9,11 @@ import Footer from './Footer';
 const Sidebar = () => {
   const handleCVDownload = () => {
     const link = document.createElement('a');
-    link.href = '/cv.pdf';
-    link.download = 'CV_YunusYildiz.pdf';
+    link.href = `${window.location.origin}/YunusYILDIZ-CV.pdf`;
+    link.download = 'YunusYILDIZ-CV.pdf';
+    document.body.appendChild(link);
     link.click();
+    document.body.removeChild(link);
   };
 
   return (
@@ -29,7 +31,7 @@ const Sidebar = () => {
         <Link to="/contact">
           <MdContactPhone size={30} color="#f56e1a" />
         </Link>
-        <Link to="#" onClick={handleCVDownload}>
+        <Link to="/cv.pdf" onClick={handleCVDownload}>
           <TbFileCv size={35} color="#f56e1a" />
         </Link>
       </nav>
