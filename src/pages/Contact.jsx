210ac25f25c9aec3f780e7c850223568228d933d
yunusyildiz-ch <yunusyildizch@gmail.com>
2025-02-09 20:19:13 +0100

    build: contact -home pages translation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa';
 import { TbWorldWww } from "react-icons/tb";
+import { useTranslation } from "react-i18next";
 
 const Contact = () => {
+  const { t } = useTranslation(); // Use i18n translation
+
   return (
     <div className="contact-container">
-      <h2>Contact Me</h2>
-      <p>You can reach me via email or connect with me on LinkedIn and GitHub.</p>
+      <h2>{t("contact.title")}</h2>
+      <p>{t("contact.description")}</p>
       <div className="social-links">
         <a href="mailto:yunusyildiz@swissmail.com" className="contact-link">
-          <FaEnvelope size={30}  />
+          <FaEnvelope size={30} />
         </a>
         <a href="https://linkedin.com/in/yunusyildiz" className="contact-link" target="_blank" rel="noopener noreferrer">
-          <FaLinkedin size={30} style={{ color: '#0077b5' }} />
+          <FaLinkedin size={30} style={{ color: "#0077b5" }} />
         </a>
         <a href="https://github.com/yunusyildiz-ch" className="contact-link" target="_blank" rel="noopener noreferrer">
-          <FaGithub size={30} style={{ color: '#333' }} />
+          <FaGithub size={30} style={{ color: "#333" }} />
         </a>
         <a href="https://yunusyildiz.ch/" className="contact-link" target="_blank" rel="noopener noreferrer">
-          <TbWorldWww size={35} style={{ color: '#f06209' }} />
+          <TbWorldWww size={35} style={{ color: "#f06209" }} />
         </a>
       </div>
     </div>
